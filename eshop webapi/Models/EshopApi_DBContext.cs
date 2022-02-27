e8ba6b54fc22787026853727b083ed9e19cb374f
nas.kh <nasser.fet@gmail.com>
2022-02-28 00:43:29 +0330

    init

---
+++ eshop webapi/Models/EshopApi_DBContext.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace eshop_webapi.Models
+{
+    public partial class EshopApi_DBContext : DbContext
+    {
+        public EshopApi_DBContext()
+        {
+        }
+
+        public EshopApi_DBContext(DbContextOptions<EshopApi_DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderItem> OrderItems { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<SalePerson> SalePeople { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=.; Initial Catalog=EshopApi_DB;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.Address).HasMaxLength(150);
+
+                entity.Property(e => e.City).HasMaxLength(150);
+
+                entity.Property(e => e.Email).HasMaxLength(150);
+
+                entity.Property(e => e.FirstName).HasMaxLength(150);
+
+                entity.Property(e => e.LastName).HasMaxLength(150);
+
+                entity.Property(e => e.Phone).HasMaxLength(150);
+
+                entity.Property(e => e.State).HasMaxLength(150);
+
+                entity.Property(e => e.ZipCode).HasMaxLength(150);
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderId).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Date).HasColumnType("datetime");
+
+                entity.Property(e => e.Status).HasMaxLength(150);
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.CustomerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Orders_Customer");
+
+                entity.HasOne(d => d.OrderNavigation)
+                    .WithOne(p => p.Order)
+                    .HasForeignKey<Order>(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Orders_SalePerson");
+            });
+
+            modelBuilder.Entity<OrderItem>(entity =>
+            {
+                entity.ToTable("OrderItem");
+
+                entity.Property(e => e.OrderItemId).ValueGeneratedOnAdd();
+
+                entity.HasOne(d => d.OrderItemNavigation)
+                    .WithOne(p => p.OrderItem)
+                    .HasForeignKey<OrderItem>(d => d.OrderItemId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_OrderItem_Orders");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderItems)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_OrderItem_Product");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.ProductName).HasMaxLength(150);
+
+                entity.Property(e => e.Status).HasMaxLength(150);
+
+                entity.Property(e => e.Varienty).HasMaxLength(150);
+            });
+
+            modelBuilder.Entity<SalePerson>(entity =>
+            {
+                entity.ToTable("SalePerson");
+
+                entity.Property(e => e.Address).HasMaxLength(150);
+
+                entity.Property(e => e.City).HasMaxLength(150);
+
+                entity.Property(e => e.Email).HasMaxLength(150);
+
+                entity.Property(e => e.FirstName).HasMaxLength(150);
+
+                entity.Property(e => e.LastName).HasMaxLength(150);
+
+                entity.Property(e => e.Phone).HasMaxLength(150);
+
+                entity.Property(e => e.State).HasMaxLength(150);
+
+                entity.Property(e => e.ZipCode).HasMaxLength(150);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
