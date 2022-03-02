6a0e902a0e6a42b6f82b5a30db4064d9056f3b5a
nas.kh <nasser.fet@gmail.com>
2022-03-03 00:33:03 +0330

    implemented CutomerRepository in web client and consume customer-Apis

--- WebClient/Models/CustomerRepository.cs
+++ WebClient/Models/CustomerRepository.cs
@@ -1,6 +1,72 @@
-﻿namespace WebClient.Models
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WebClient.Models
 {
-    public class CustomerRepository
+    public class CustomerRepository : ICustomerRepository
     {
+        private string apiUrl = "http://localhost:3962/api/customers";
+        private HttpClient _client;
+
+        public CustomerRepository()
+        {
+            _client = new HttpClient();
+        }
+
+        public List<Customer> GetAllCustomer()
+        {
+            var result = _client.GetStringAsync(apiUrl).Result;
+            List<Customer> list = JsonConvert.DeserializeObject<List<Customer>>(result);
+            return list;
+        }
+
+        public Customer GetCustomerById(int customerId)
+        {
+            var result = _client.GetStringAsync(apiUrl + "/" + customerId).Result;
+            Customer customer = JsonConvert.DeserializeObject<Customer>(result);
+
+            return customer;
+        }
+
+        public HttpResponseMessage AddCustomer(Customer customer)
+        {
+            string jsonCustomer = JsonConvert.SerializeObject(customer);
+            StringContent content = new StringContent(jsonCustomer, Encoding.UTF8, "application/json");
+
+            var result = _client.PostAsync(apiUrl, content).Result;
+            return result;
+        }
+
+        public HttpResponseMessage UpdateCustomer(Customer customer)
+        {
+            string jsonCustomer = JsonConvert.SerializeObject(customer);
+            StringContent content = new StringContent(jsonCustomer, Encoding.UTF8, "application/json");
+
+            var result = _client.PutAsync(apiUrl, content).Result;
+            return result;
+        }
+
+        public HttpResponseMessage DeleteCustomer(int customerId)
+        {
+            var result = _client.DeleteAsync(apiUrl + "/" + customerId).Result;
+            return result;
+
+        }
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
     }
 }
