84d91f7fc04e7b20bb1788438ddf19021ffd9e84
nas.kh <nasser.fet@gmail.com>
2022-03-13 00:14:36 +0330

    project repository pattern added

---
+++ DAL/ProjectContext.cs
@@ -0,0 +1,15 @@
+﻿using DomainClassModel.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class ProjectContext : DbContext
+    {
+        public DbSet<Person> Person { get; set; }
+    }
+}
---
+++ DomainModel/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainClassModel.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string LastName { get; set; }
+        [MaxLength(150)]
+        public string WebSite{ get; set; }
+    }
+}
