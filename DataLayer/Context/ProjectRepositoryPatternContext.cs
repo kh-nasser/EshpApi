5f632c0d4e3e3ac88326b4a38b974776ff6c4f19
nas.kh <nasser.fet@gmail.com>
2022-03-17 00:55:38 +0330

    created repository pattern sample installed entity framework core packages
    codeFirst-db-init
     Add-Migration

---
+++ DataLayer/Context/ProjectRepositoryPatternContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectRepositoryPattern_ModelClass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectRepositoryPattern_DataLayer.Context
+{
+    public class ProjectRepositoryPatternContext : DbContext
+    {
+        public ProjectRepositoryPatternContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public ProjectRepositoryPatternContext()
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+       
+    }
+}
--- ModelClass/Models/Person.cs
+++ ModelClass/Models/Person.cs
@@ -5,19 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DomainClassModel.Models
+namespace ProjectRepositoryPattern_ModelClass.Models
 {
     public class Person
     {
         [Key]
-        public int PersonId { get; set; }
+        public int PersonID { get; set; }
         [Required]
         [MaxLength(150)]
         public string Name { get; set; }
+        [MaxLength(150)]
         [Required]
+        public string Family { get; set; }
         [MaxLength(150)]
-        public string LastName { get; set; }
-        [MaxLength(150)]
-        public string WebSite{ get; set; }
+        public string WebSite { get; set; }
+
     }
 }
