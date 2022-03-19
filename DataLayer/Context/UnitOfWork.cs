c5f5f15aa4de89151fe745cbf9d854fed9be1e65
nas.kh <nasser.fet@gmail.com>
2022-03-19 16:06:02 +0330

    added UnitOfWork

---
+++ DataLayer/Context/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using ProjectRepositoryPattern_DataLayer.Services;
+using ProjectRepositoryPattern_ModelClass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectRepositoryPattern_DataLayer.Context
+{
+    public class UnitOfWork : IDisposable
+    {
+        ProjectRepositoryPatternContext db = new ProjectRepositoryPatternContext();
+
+        #region Person
+        private MyGenericRepository<Person> personRepository;
+        public MyGenericRepository<Person> PersonRepository
+        {
+            get
+            {
+                if (personRepository == null)
+                {
+                    personRepository = new MyGenericRepository<Person>(db);
+                }
+                return personRepository;
+            }
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
