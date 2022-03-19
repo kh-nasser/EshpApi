faefda84c95281881fd7c308b46d368aeb10f4f4
nas.kh <nasser.fet@gmail.com>
2022-03-19 15:52:46 +0330

    added GenericRepository

---
+++ DataLayer/Repository/IMyGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectRepositoryPattern_DataLayer.Repository
+{
+    public interface IMyGenericRepository<TEntity> where TEntity : class
+    {
+        void Delete(object id);
+        void Delete(TEntity entity);
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> conditionWhere = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string[] includesRelatioship = null);
+        TEntity GetById(object id);
+        void Insert(TEntity entity);
+        void Save();
+        void Update(TEntity entity);
+    }
+}
